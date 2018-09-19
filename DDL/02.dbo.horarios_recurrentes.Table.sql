87c60f5b9e34bc870fe9c6cd76f4a09e94bc0564
MariaCoria <42949935+MariaCoria@users.noreply.github.com>
2018-09-19 15:24:41 -0300

    Drop table horarios_recurrentes

--- DDL/02.dbo.horarios_recurrentes.Table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE [dbo].[horarios_recurrentes](
-	[id_servicio_profesional] [int] NULL,
-	[es_profesional] [bit] NULL,
-	[tipo_repeticion] [char](1) NULL,
-	[cantidad_repeticiones] [int] NULL,
-	[fecha_hora_inicio] [datetime] NULL,
-	[fecha_hora_fin] [datetime] NULL
-) ON [PRIMARY]
