f0203a5530f9b900e63f73f3cdc0eaa99355629f
MariaCoria <42949935+MariaCoria@users.noreply.github.com>
2018-09-19 16:24:43 -0300

    Cambios en la lógica del procedure.

--- Desarrollo/02.dbo.alta_de_horarios_recu.StoredProcedure.sql
+++ Desarrollo/02.dbo.alta_de_horarios_recu.StoredProcedure.sql
@@ -1,19 +1,32 @@
-CREATE PROCEDURE [dbo].[alta_de_horarios_recu]
-@id_servicio_profesional int,
-@es_profesional bit,
-@tipo_repeticion char(1),
-@cantidad_repeticiones int,
-@fecha_hora_inicio datetime,
-@fecha_hora_fin datetime
-	
+CREATE PROCEDURE alta_de_horarios_recu
+	@es_profesional bit,
+	@tipo_repeticion char(1),
+	@cantidad_repeticiones int,
+	@fecha_hora_inicio datetime,
+	@fecha_hora_fin datetime
 AS
-	BEGIN
+BEGIN
+	DECLARE @contador int
+	SET @contador = 0
 
-		insert into horarios_recurrentes (id_servicio_profesional,es_profesional,
-										  tipo_repeticion,cantidad_repeticiones,
-										  fecha_hora_inicio,fecha_hora_fin)
-		values (@id_servicio_profesional,@es_profesional,@tipo_repeticion,@cantidad_repeticiones,
-				@fecha_hora_inicio,@fecha_hora_fin);
-		
-	END
-GO
+	WHILE (@contador != @cantidad_repeticiones)
+		BEGIN 
+			IF (@tipo_repeticion = 'c')
+				INSERT INTO Horarios_disponibles (es_profesional, fecha_hora_inicio, fecha_hora_fin)
+									VALUES (@es_profesional, DATEADD(dd, @contador, @fecha_hora_inicio), DATEADD(dd, @contador, @fecha_hora_fin))
+			IF (@tipo_repeticion = 'l')
+				INSERT INTO Horarios_disponibles (es_profesional, fecha_hora_inicio, fecha_hora_fin)
+									VALUES (@es_profesional, DATEADD(dw, @contador, @fecha_hora_inicio), DATEADD(dw, @contador, @fecha_hora_fin))
+				
+			IF (@tipo_repeticion = 's')
+				INSERT INTO Horarios_disponibles (es_profesional, fecha_hora_inicio, fecha_hora_fin)
+									VALUES (@es_profesional, DATEADD(wk, @contador, @fecha_hora_inicio), DATEADD(wk, @contador, @fecha_hora_fin))
+
+			IF (@tipo_repeticion = 'm')
+				INSERT INTO Horarios_disponibles (es_profesional, fecha_hora_inicio, fecha_hora_fin)
+									VALUES (@es_profesional, DATEADD(mm, @contador, @fecha_hora_inicio), DATEADD(mm, @contador, @fecha_hora_fin))
+
+			SET @contador = @contador+1
+		END
+		PRINT 'Se han dado de alta los horarios recurrentes'
+END
