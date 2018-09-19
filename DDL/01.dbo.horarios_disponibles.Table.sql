d2d55256ee9bb01954771376020dd4620df6ef13
MariaCoria <42949935+MariaCoria@users.noreply.github.com>
2018-09-19 16:50:44 -0300

    Update 01.dbo.horarios_disponibles.Table.sql

--- DDL/01.dbo.horarios_disponibles.Table.sql
+++ DDL/01.dbo.horarios_disponibles.Table.sql
@@ -2,7 +2,5 @@ CREATE TABLE [dbo].[horarios_disponibles](
 	[id_servicio_profesional] [int] NULL,
 	[es_profesional] [bit] NULL,
 	[fecha_hora_inicio] [datetime] NULL,
-	[fecha_hora_fin] [datetime] NULL,
-	[activo] [bit] NULL
-		    
+	[fecha_hora_fin] [datetime] NULL		    
 )
